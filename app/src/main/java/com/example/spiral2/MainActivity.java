95e0d3d1440e93f3149d792df3e0b2b79927edab
张开山 <zhangkaishan@yamas-MacBook-Pro.local>
2020-03-01 17:13:01 -0500

    add uid list to the test mode

--- app/src/main/java/com/example/spiral2/MainActivity.java
+++ app/src/main/java/com/example/spiral2/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.spiral2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+
+        //go to test mode
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        test = findViewById(R.id.test);
+
     }
 }
